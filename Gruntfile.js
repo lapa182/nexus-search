d779ffab09828737a53df735886d740d63bc2db0
Igo Lapa <igo@gabstr.com>
2014-09-01 09:29:04 -0300

    uploading riot html file and other stuffs.

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function (grunt) {
 
         // Watch Config
         watch: {
-            files: ['views/**/*'],
+            files: ['views/**/*', 'routes/**/*'],
             options: {
                 livereload: true
             },
@@ -34,7 +34,7 @@ module.exports = function (grunt) {
                 ],
             },
             express: {
-                files:  [ 'app.js', '!**/node_modules/**', '!Gruntfile.js' ],
+                files:  [ 'app.js', './routes/**/*', '!**/node_modules/**', '!Gruntfile.js' ],
                 tasks:  [ 'express:dev' ],
                 options: {
                     nospawn: true // Without this option specified express won't be reloaded
--- app.js
+++ app.js
@@ -6,10 +6,12 @@
 var express     = require('express'),
     hbs         = require('express-hbs'),
     serveStatic = require('serve-static'),
+    riotKey			= '17a15899-76bb-4317-8227-baa1ad50bc1b',
     compression = require('compression'),
     app         = express(),
     port        = 3000;
 
+require('./routes/nexus_route.js')(app);
 // For gzip compression
 app.use(compression());
 
@@ -17,6 +19,8 @@ app.engine('hbs', hbs.express3({
   partialsDir: __dirname + '/views/partials',
   defaultLayout: __dirname +'/views/layouts/main.hbs',
 }));
+
+app.use(express.static(__dirname + '/assets'));
 
 // Set Handlebars
 app.set('view engine', 'hbs');
