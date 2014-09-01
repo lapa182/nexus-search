9bca3497fd0b92b18306c7a1168c5f510e9353d3
Igo Lapa <igo@gabstr.com>
2014-09-01 10:42:30 -0300

    deploying for error log.

--- routes/nexus_route.js
+++ routes/nexus_route.js
@@ -1,13 +1,11 @@
 module.exports = function(app, request, riotKey) {
-  app.get('/form-get', function(req, response) {
+  app.get('/summoner', function(req, response) {
  	var request  = require('request-json');
 	var summoner = req.query['summoner'],
 			url 		 = 
 			client 	 = request.newClient('https://br.api.pvp.net/api/lol/br/v1.4/summoner/by-name/Venelope?api_key=c9ebfedc-13c6-4810-bb6b-571d73c4e0a1');
-			console.log(summoner)
-			response.redirect('/search');
 		client.get('/search', function(err, res, body) {
-			return res.send(body);
+			return response.send(body);
 			// response.redirect('/search');
 		});
   });
